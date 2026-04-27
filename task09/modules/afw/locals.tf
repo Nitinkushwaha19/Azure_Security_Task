f13b812f92ea291d53f3f2ce461cb3195a3e1374
nitinkushwaha <nitinkushwaha706@outlook.com>
2026-04-27 19:10:45 +0530

    fixed the afw subnet name

--- task09/modules/afw/locals.tf
+++ task09/modules/afw/locals.tf
@@ -1,9 +1,9 @@
 locals {
   name_prefix = var.name_prefix
 
-  afw_name           = "AzureFirewallSubnet"
+  afw_name           = join("", [local.name_prefix, "afw"])
   afw_sku_name       = "AZFW_VNet"
-  afw_subnet_name    = join("", [local.name_prefix, "afw-snet"])
+  afw_subnet_name    = "AzureFirewallSubnet"
   afw_public_ip_name = join("", [local.name_prefix, "pip"])
   route_table_name   = join("", [local.name_prefix, "route-table"])
   route_name         = join("", [local.name_prefix, "route-afw"])
